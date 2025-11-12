4e9cca498705387894c4c1a04e6bfbaf38363446
Ayush <imayush2702@gmail.com>
2025-11-12 11:45:51 +0530

    Assignment push

---
+++ Js-Assignment-1/Q2.js
@@ -0,0 +1,16 @@
+let name = "Ayush";
+let age = 25;
+let isStudent = true;
+let hobbies = ["music", "BasketBAll"];
+let profile = { city: "Paris", id: 123 };
+let emptyValue = null;
+let notDefined;
+console.table([
+  { Label: "Name", Value: name, Type: typeof name },
+  { Label: "Age", Value: age, Type: typeof age },
+  { Label: "Is Student", Value: isStudent, Type: typeof isStudent },
+  { Label: "Hobbies", Value: hobbies, Type: Array.isArray(hobbies) ? "array" : typeof hobbies },
+  { Label: "Profile", Value: JSON.stringify(profile), Type: typeof profile },
+  { Label: "Empty Value", Value: emptyValue, Type: typeof emptyValue },
+  { Label: "Not Defined", Value: notDefined, Type: typeof notDefined }
+]);
